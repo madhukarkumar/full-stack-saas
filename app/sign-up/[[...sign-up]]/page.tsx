62c82c2c060541690481031501045de7957920ea
Madhukar Kumar <madhukar.kumar@gmail.com>
2024-09-08 11:43:29 -0700

    Design fix
    Center aligned the sign up page component

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function SignUpPage() {
-  return <SignUp />;
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <SignUp />
+    </div>
+  );
 }
